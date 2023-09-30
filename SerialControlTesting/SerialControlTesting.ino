91040654424d003b1409a723a7f63ae633799748
Sailboy42 <ohimsworth@olin.edu>
2023-09-30 16:40:01 -0400

    Add Serial Testing Code
    Added code to and link to test inputting data through serial. Code here is able to move servo.

---
+++ SerialControlTesting/SerialControlTesting.ino
@@ -0,0 +1,60 @@
+//https://forum.arduino.cc/t/serial-input-basics-updated/382007/2
+
+// Example 4 - Receive a number as text and convert it to an int
+
+const byte numChars = 32;
+char receivedChars[numChars];   // an array to store the received data
+
+boolean newData = false;
+
+int dataNumber = 0;             // new for this version
+#include <Servo.h>
+
+
+Servo myservo;  // create servo object to control a servo
+void setup() {
+    Serial.begin(9600);
+    Serial.println("<Arduino is ready>");
+    myservo.attach(7);
+}
+
+void loop() {
+    recvWithEndMarker();
+    showNewNumber();
+}
+
+void recvWithEndMarker() {
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+    
+    if (Serial.available() > 0) {
+        rc = Serial.read();
+
+        if (rc != endMarker) {
+            receivedChars[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars) {
+                ndx = numChars - 1;
+            }
+        }
+        else {
+            receivedChars[ndx] = '\0'; // terminate the string
+            ndx = 0;
+            newData = true;
+        }
+    }
+}
+
+void showNewNumber() {
+    if (newData == true) {
+        dataNumber = 0;             // new for this version
+        dataNumber = atoi(receivedChars);   // new for this version
+        Serial.print("This just in ... ");
+        Serial.println(receivedChars);
+        Serial.print("Data as Number ... ");    // new for this version
+        Serial.println(dataNumber);     // new for this version
+        newData = false;
+        myservo.write(dataNumber);
+    }
+}
