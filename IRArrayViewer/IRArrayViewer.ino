8e08c920b9c2d8ec995a535bf31f1a8d9a7a050a
Sailboy42 <ohimsworth@olin.edu>
2023-10-11 14:37:39 -0400

    Add sum array value to arrayviewer
    Added a calculation for getting sum ir data in the array viewer and print statment to view it

--- IRArrayViewer/IRArrayViewer.ino
+++ IRArrayViewer/IRArrayViewer.ino
@@ -8,18 +8,19 @@ void setup() {
 }
 
 void loop() {
+  int irSum = 0;
   //Print in Serial IR readings
   //Cycle through sensors and assigns them to array. Left to right
   for (int i = 0; i < NUM_SENSORS; i++){
     irData[i] = int(analogRead[i]);
     Serial.print((String)"IR "+i+": "+irData[i]);
+    irSum += int(irData[i]);
   }
 
   //Prints target reading
   int v;
   v = (4000*irData[0] + 3000*irData[1] + 2000*irData[2] + 1000*irData[3] + 0*irData[4])/(irData[0] + irData[1] + irData[2] + irData[3] + irData[4]);
-  Serial.print("Position = ");
-  Serial.println(v);
+  Serial.print((String)"Position = " +v+ ", IR Sum: " +irSum);
 
   delay(250);
 }
