af0ac643bfb82281161bd9598f466d3ee94e8600
Sailboy42 <ohimsworth@olin.edu>
2023-10-12 13:35:49 -0400

    Increase Delay in IR Viewer and Fix Values
    Increased delay in in IR Viewer to slow down output into serial. Also changed analog read from [] to () which was causing an error in data values read.

--- IRArrayViewer/IRArrayViewer.ino
+++ IRArrayViewer/IRArrayViewer.ino
@@ -12,15 +12,16 @@ void loop() {
   //Print in Serial IR readings
   //Cycle through sensors and assigns them to array. Left to right
   for (int i = 0; i < NUM_SENSORS; i++){
-    irData[i] = int(analogRead[i]);
-    Serial.print((String)"IR "+i+": "+irData[i]);
+    irData[i] = analogRead(i);
+    //Serial.println(analogRead(i));
+    Serial.println((String)"IR "+i+": "+irData[i]);
     irSum += int(irData[i]);
   }
 
   //Prints target reading
   int v;
   v = (4000*irData[0] + 3000*irData[1] + 2000*irData[2] + 1000*irData[3] + 0*irData[4])/(irData[0] + irData[1] + irData[2] + irData[3] + irData[4]);
-  Serial.print((String)"Position = " +v+ ", IR Sum: " +irSum);
+  Serial.println((String)"Position = " +v+ ", IR Sum: " +irSum);
 
-  delay(250);
+  delay(600);
 }
