0236b4f0ce2be744cc770c89f09c16a49938b0b6
Sailboy42 <ohimsworth@olin.edu>
2023-10-17 13:52:19 -0400

    Fix importing mess up
    idk it didnt work messed with order

--- ImportSave.py
+++ ImportSave.py
@@ -15,6 +15,7 @@ def serial2_csv(arduino_com_port="COM3", baud_rate=9600, file_path="serial_data.
         NA
     """
     serial_port = serial.Serial(arduino_com_port, baud_rate, timeout=1)
+    lf_data = pd.DataFrame()
 
     while True:
         #
@@ -27,27 +28,40 @@ def serial2_csv(arduino_com_port="COM3", baud_rate=9600, file_path="serial_data.
         # check if data was received
         #
         if len(scan_data) > 0:
+            # print(scan_data)
+            scan_data = list(scan_data.split(","))
+            scan_data = [x.rstrip() for x in scan_data]
+            # print(type(scan_data))
+            print(scan_data)
             df = pd.DataFrame(scan_data)  # convert list to dataframe
+            print(df)
+            lf_data = lf_data.append(df, ignore_index=True)
 
-            """***************************************************************************************
-            *    Title: Pandas tocsv Checking for Overwrite Using Mode Paramter
-            *    Author: Saturn Cloud
-            *    Date: Monday, June 19, 2023
-            *    Code version: 1
-            *    Availability: https://saturncloud.io/blog/pandas-tocsv-checking-for-overwrite/
-            *
-            ***************************************************************************************"""
+        lf_data.to_csv(lf_data, sep=",", index=False, encoding="utf-8")
 
-            try:
-                df.to_csv(file_path, index=False, mode="x")
-                print(f"The file '{file_path}' has been created.")
-            except FileExistsError:
-                overwrite = input(
-                    f"The file '{file_path}' already exists. Do you want to overwrite it? (y/n): "
-                )
-                if overwrite.lower() == "y":
-                    df.to_csv(file_path, index=False, mode="w")
-                    print(f"The file '{file_path}' has been overwritten.")
-                else:
-                    print(f"The file '{file_path}' has not been overwritten.")
+        """***************************************************************************************
+        *    Title: Pandas tocsv Checking for Overwrite Using Mode Paramter
+        *    Author: Saturn Cloud
+        *    Date: Monday, June 19, 2023
+        *    Code version: 1
+        *    Availability: https://saturncloud.io/blog/pandas-tocsv-checking-for-overwrite/
+        *
+        ***************************************************************************************"""
+        """
+        try:
+            df.to_csv(file_path, index=False, mode="x")
+            print(f"The file '{file_path}' has been created.")
+        except FileExistsError:
+            overwrite = input(
+                f"The file '{file_path}' already exists. Do you want to overwrite it? (y/n): "
+            )
+            if overwrite.lower() == "y":
+                df.to_csv(file_path, index=False, mode="w")
+                print(f"The file '{file_path}' has been overwritten.")
+            else:
+                print(f"The file '{file_path}' has not been overwritten.")
+        """
     return
+
+
+serial2_csv(arduino_com_port="COM7", file_path="serial_data_test.csv")
--- Line_following_nonPID/Line_following_nonPID.ino
+++ Line_following_nonPID/Line_following_nonPID.ino
@@ -3,43 +3,50 @@
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 // Setting up Motorshield
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-Adafruit_DCMotor *leftMotor = AFMS.getMotor(3); // we are using M3 port
-Adafruit_DCMotor *rightMotor = AFMS.getMotor(2); // we are using M1 port
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(3);   // we are using M3 port
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);  // we are using M1 port
 
-
-#define leftSensor  A2 
+//sensor pins
+#define leftSensor A2
 #define centerSensor A1
-#define rightSensor  A0     // sensors defined
-#define L_MOTOR 6 //Left motor pin
-#define R_MOTOR 7  //Right motor pin
+#define rightSensor A0
+
+//Motor pins
+#define L_MOTOR 6
+#define R_MOTOR 7
 #define threshold 850
 #define motor_speed 30
+
 int lastAction = 0;
+
+//Motor variables
 int motorSpeed = 25;
 int speedReverse = -1;
 int speedLeft = 25;
 int speedRight = 25;
 
-bool start = true;
+bool start = true;  //Start condition
 
 //Serial control setup
 const byte numChars = 32;
-char receivedChars[numChars];   // an array to store the received data
+char receivedChars[numChars];  // an array to store the received data
 boolean newData = false;
 
 void setup() {
-  AFMS.begin(); // initialise adafruit library
+  AFMS.begin();  // initialise adafruit library
+
   // Initialize Pins
   pinMode(L_MOTOR, OUTPUT);
   pinMode(R_MOTOR, OUTPUT);
   pinMode(leftSensor, INPUT);
   pinMode(centerSensor, INPUT);
   pinMode(rightSensor, INPUT);
-  Serial.begin(9600); //Start serial
-  
+
+  Serial.begin(9600);  //Start serial
+
   // set motor sppeds and directions
-  leftMotor->setSpeed(30); // initialise the speed + direction for left
+  leftMotor->setSpeed(30);  // initialise the speed + direction for left
   leftMotor->run(BACKWARD);
 
   rightMotor->setSpeed(30);  // initialise the speed + direction for motor right
@@ -59,177 +66,153 @@ void loop() {
   char startMarker = '<';
   char endMarker = '>';
   char rc;
-    
+
   while (Serial.available() > 0 && newData == false) {
-      rc = Serial.read();
+    rc = Serial.read();
 
-      if (recvInProgress == true) {
-          if (rc != endMarker) {
-              receivedChars[ndx] = rc;
-              ndx++;
-              if (ndx >= numChars) {
-                  ndx = numChars - 1;
-              }
-          }
-          else {
-              receivedChars[ndx] = '\0'; // terminate the string
-              recvInProgress = false;
-              ndx = 0;
-              newData = true;
-          }
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      } else {
+        receivedChars[ndx] = '\0';  // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
       }
+    }
 
-      else if (rc == startMarker) {
-          recvInProgress = true;
-      }
+    else if (rc == startMarker) {
+      recvInProgress = true;
+    }
   }
   //Show condition being applied to LF program and prints to serial
   if (newData == true) {
-        Serial.print("Command: ");
-        Serial.println(receivedChars);
-        String command = String(receivedChars);
+    Serial.print("Command: ");
+    Serial.println(receivedChars);
+    String command = String(receivedChars);
 
-        if (command == "STOP") {
-          start = false;
-        }
-        else if (command == "START") {
-          start = true;
-        }
-        else {
-          Serial.println("Invalid Command");
-          
-        }
-        newData = false;
+    if (command == "STOP") {
+      start = false;
+    } else if (command == "START") {
+      start = true;
+    } else {
+      Serial.println("Invalid Command");
+    }
+    newData = false;
   }
+
   // Line Follower Loop
-int leftValue = analogRead(leftSensor); // sensor values
-int centerValue = analogRead(centerSensor);
-int rightValue = analogRead(rightSensor);
-Serial.println((String)leftValue+","+centerValue+","+rightValue+ ","+ speedLeft+","+speedRight);
+  int leftValue = analogRead(leftSensor);  // sensor values
+  int centerValue = analogRead(centerSensor);
+  int rightValue = analogRead(rightSensor);
+  Serial.println((String)leftValue + "," + centerValue + "," + rightValue + "," + speedLeft + "," + speedRight + "," + startTime);
 
+  // if side sensors detect floor and middle detects tape move forward
+  if ((leftValue < threshold) && (centerValue > threshold) && (rightValue < threshold)) {
+    // Go forward
+    leftMotor->setSpeed(motorSpeed);
+    rightMotor->setSpeed(motorSpeed);
+    speedLeft = motorSpeed;
+    speedRight = motorSpeed;
 
+    lastAction = 0;
+  }
 
-// if side sensors detect floor and middle detects tape move forward
-if ((leftValue < threshold) && (centerValue > threshold) && (rightValue < threshold)) {
-  // Go forward
-  leftMotor->setSpeed(motorSpeed);
-  rightMotor->setSpeed(motorSpeed);
-  speedLeft = motorSpeed;
-  speedRight = motorSpeed;
-
-  lastAction = 0;
-}
-// If left sensor senses tape and center & left don't
-if ((leftValue > threshold) && (centerValue < threshold) && (rightValue < threshold)){
-  // turn right
-  leftMotor->run(BACKWARD);
-  rightMotor->run(FORWARD);
-  leftMotor->setSpeed(motorSpeed);
-  rightMotor->setSpeed(motorSpeed);
-  lastAction = 1;
-  speedLeft = motorSpeed * -1;
-  speedRight = motorSpeed;
-
-}
-// If right sensor senses tape and the others don't
-if ((leftValue < threshold) && (centerValue < threshold) && (rightValue > threshold)){
-  // turn left
-  leftMotor->run(FORWARD);
-  rightMotor->run(BACKWARD);
-  leftMotor->setSpeed(motorSpeed);
-  rightMotor->setSpeed(motorSpeed);
-  lastAction = 2;
-  speedLeft = motorSpeed;
-  speedRight = motorSpeed * -1;
-}
-
-// if middle and left sensor sense tape
-if ((leftValue > threshold) && (centerValue > threshold) && (rightValue < threshold)){
-  leftMotor->run(BACKWARD);
-  rightMotor->run(FORWARD);
-  leftMotor->setSpeed(motorSpeed);
-  rightMotor->setSpeed(motorSpeed);
-  lastAction = 1;
-  delay(200);
-  speedLeft = motorSpeed*-1;
-  speedRight = motorSpeed;
-}
-
-if ((leftValue < threshold) && (centerValue > threshold) && (rightValue > threshold)){
-  leftMotor->run(FORWARD);
-  rightMotor->run(BACKWARD);
-  leftMotor->setSpeed(motorSpeed);
-  rightMotor->setSpeed(motorSpeed);
-  lastAction = 2;
-  speedLeft = motorSpeed;
-  speedRight = motorSpeed*-1;
-  delay(200);
-}
-// if no sensors sense tape
-if ((leftValue < threshold) && (centerValue < threshold) && (rightValue < threshold)) {
-  // stop
-  if (lastAction == 1){
+  // if drifting to left correct to right
+  if ((leftValue > threshold) && (centerValue < threshold) && (rightValue < threshold)) {
+    // turn right
     leftMotor->run(BACKWARD);
     rightMotor->run(FORWARD);
     leftMotor->setSpeed(motorSpeed);
     rightMotor->setSpeed(motorSpeed);
-    speedLeft = motorSpeed*-1;
+    lastAction = 1;
+    speedLeft = motorSpeed * -1;
     speedRight = motorSpeed;
   }
-  if (lastAction == 2){
+
+  //if drifting to right correct to left
+  if ((leftValue < threshold) && (centerValue < threshold) && (rightValue > threshold)) {
+    // turn left
     leftMotor->run(FORWARD);
     rightMotor->run(BACKWARD);
     leftMotor->setSpeed(motorSpeed);
     rightMotor->setSpeed(motorSpeed);
+    lastAction = 2;
     speedLeft = motorSpeed;
-    speedRight = motorSpeed*-1;
+    speedRight = motorSpeed * -1;
   }
-  else {
-  leftMotor->run(BACKWARD);
-  leftMotor->setSpeed(motorSpeed);
-  rightMotor->run(BACKWARD);
-  rightMotor->setSpeed(motorSpeed);
-  speedLeft = motorSpeed;
-  speedRight = motorSpeed;
-  lastAction = 3;
+
+  //If course is to right go right
+  if ((leftValue > threshold) && (centerValue > threshold) && (rightValue < threshold)) {
+    leftMotor->run(BACKWARD);
+    rightMotor->run(FORWARD);
+    leftMotor->setSpeed(motorSpeed);
+    rightMotor->setSpeed(motorSpeed);
+    lastAction = 1;
+    delay(200);
+    speedLeft = motorSpeed * -1;
+    speedRight = motorSpeed;
   }
-  
+
+  //If course is to left, go left
+  if ((leftValue < threshold) && (centerValue > threshold) && (rightValue > threshold)) {
+    leftMotor->run(FORWARD);
+    rightMotor->run(BACKWARD);
+    leftMotor->setSpeed(motorSpeed);
+    rightMotor->setSpeed(motorSpeed);
+    lastAction = 2;
+    speedLeft = motorSpeed;
+    speedRight = motorSpeed * -1;
+    delay(200);
+  }
+
+  // if no sensors sense tape, continue doing previous motion
+  if ((leftValue < threshold) && (centerValue < threshold) && (rightValue < threshold)) {
+    // stop
+    if (lastAction == 1) {
+      leftMotor->run(BACKWARD);
+      rightMotor->run(FORWARD);
+      leftMotor->setSpeed(motorSpeed);
+      rightMotor->setSpeed(motorSpeed);
+      speedLeft = motorSpeed * -1;
+      speedRight = motorSpeed;
+    }
+    if (lastAction == 2) {
+      leftMotor->run(FORWARD);
+      rightMotor->run(BACKWARD);
+      leftMotor->setSpeed(motorSpeed);
+      rightMotor->setSpeed(motorSpeed);
+      speedLeft = motorSpeed;
+      speedRight = motorSpeed * -1;
+    } else {
+      leftMotor->run(BACKWARD);
+      leftMotor->setSpeed(motorSpeed);
+      rightMotor->run(BACKWARD);
+      rightMotor->setSpeed(motorSpeed);
+      speedLeft = motorSpeed;
+      speedRight = motorSpeed;
+      lastAction = 3;
+    }
+  }
+
+  //Turn off start condition when stop point is reached
+  if ((leftValue > threshold) && (centerValue > threshold) && (rightValue > threshold)) {
+    start = false;
+  }
+
+  //Stop
+  if (start == false) {
+    //analogWrite(L_MOTOR, 0);
+    leftMotor->run(BACKWARD);
+    leftMotor->setSpeed(0);
+    rightMotor->run(BACKWARD);
+    rightMotor->setSpeed(0);
+    speedLeft = 0;
+    speedRight = 0;
+    lastAction = 4;
+  }
 }
-if ((leftValue > threshold) && (centerValue > threshold) && (rightValue > threshold)) {
-  start = false;
-
-}
-if (start == false) {
-  // stop
-      //analogWrite(L_MOTOR, 0);
-  leftMotor->run(BACKWARD);
-  leftMotor->setSpeed(0);
-  rightMotor->run(BACKWARD);
-  rightMotor->setSpeed(0);
-  speedLeft = 0;
-  speedRight = 0;
-  lastAction = 4;
-}
-/*
-if (start == true) {
-  //Send data over serial
-  int currentTime = millis() - startTime;
-  Serial.print(leftValue);
-  Serial.print(",");
-  Serial.print(centerValue);
-  Serial.print(",");
-  Serial.print();
-  Serial.print(",");
-  Serial.print(lmSpeed);
-  Serial.print(",");
-  Serial.print(rmSpeed);
-  Serial.print(",");
-  Serial.println(currentTime);
-}
-*/
-
-
-
-}
-
-
